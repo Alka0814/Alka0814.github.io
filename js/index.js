0193bef0b512e42619c0bfe83bfc7eb6664c0963
alka0814 <alkatrivedi0814@gmail.com>
2023-11-09 17:40:20 +0530

    done

--- js/index.js
+++ js/index.js
@@ -48,13 +48,13 @@ window.addEventListener('scroll', scrollActive)
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 1000,
+    duration: 2000,
     delay: 200,
 //     reset: true
 });
 
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 300}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills-card, .project-card, .contact__input, .stats_card, .contact_tag',{interval: 200}); 
 
